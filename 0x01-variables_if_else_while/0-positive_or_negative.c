eea2f3000c4838334ba8650c84768c4e158eb4bc
Akuda061 <arseniomouton@gmail.com>
2022-10-16 02:20:38 -0700

    if statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,20 +18,11 @@
 	 *
 	 * 	 	 	 	 	 	 * with beginning and ending al
 */
-
-
 /* betty style doc for function main goes there */
-
-int main(void)
-
+int main(void) /*main function */
 {
-
 	int n;
-
-
-
 	srand(time(0));
-
 	n = rand() - RAND_MAX / 2;
 	if (n < 0)
 	{
@@ -45,6 +36,5 @@ int main(void)
 	{
 		printf("%d is zero\n",n);
 	}
-	
-	return (0);
+		return (0);
 }
