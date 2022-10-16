b874351c3a4aba38f475ff8ecf7313502a506f7e
Akuda061 <arseniomouton@gmail.com>
2022-10-16 02:32:59 -0700

    if statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,40 +1,35 @@
 #include <stdlib.h>
 #include <stdio.h>
-
 #include <time.h>
 
-/* more headers goes there */
-	/**
-	 *
-	 * 	 * This is the preferred style for multi-line
-	 *
-	 * 	 	 * comments in C source code.
-	 *
-	 * 	 	 	 * Please use it consistently.
-	 *
-	 * 	 	 	 	 *
-	 *
-	 * 	 	 	 	 	 * Description:  A column of asterisks on the left side,
-	 *
-	 * 	 	 	 	 	 	 * with beginning and ending al
-*/
-/* betty style doc for function main goes there */
-int main(void) /*main function */
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+int main(void)
+
 {
-	int n;
+       	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n < 0)
 	{
-	       	printf("%d is negative\n",n);
+	       	printf("%d is negative\n" , n);
 	}
 	else if (n > 0)
 	{
-		printf("%d is positive\n",n);
+		printf("%d is positive\n" , n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n",n);
+		printf("%d is zero\n" , n);
 	}
-		return (0);
+	return (0);
 }
