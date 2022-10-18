52b324d072284f76467947fc89f85d54201f58ff
Akuda061 <arseniomouton@gmail.com>
2022-10-18 09:25:43 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - is the main program strating point
+ *
+ * Return: Always return  0 for (Succes)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
