02f77d4cbc19b3f933724f7f00b803bba580b783
Akuda061 <arseniomouton@gmail.com>
2022-10-19 11:29:41 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+
+#include "main.h"
+
+/**
+ * print_alphabet - the starting point
+ *
+ * Return: after program is done it return
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i < 'z'; i++)
+	_putchar(i);
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - starting point
+ *
+ * Return: returning 0 will be succes
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
